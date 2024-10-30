4f818316a7b999d20950c26e66feb8aea8393bf0
FabianRapp <fa.rapp@web.de>
2024-10-30 17:01:41 +0100

    started isdigit.s, tho paused for now

---
+++ src/isdigit.s
@@ -0,0 +1,28 @@
+#int	ft_isdigit(int c);
+.global ft_isdigit
+
+.equ _CHAR_0, 48
+.equ _CHAR_9, 57
+
+ft_isdigit:
+#if c - '0' < 0 return false
+	mov $(_CHAR_0), %rbx
+	cmp %rbx, %rdi
+	jl return_false
+
+#else if c - '9' > 0 return false
+	mov $(_CHAR_9), %rbx
+	cmp %rbx, %rdi
+	jg return_false
+#else
+	jmp return_true
+
+
+return_false:
+	mov $0, %rax
+	ret
+
+return_true:
+	mov $1, %rax
+	ret
+
