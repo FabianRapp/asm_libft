cd0275d40883834f61645bfb7e4b66cc8820bff6
FabianRapp <fa.rapp@web.de>
2024-11-01 16:38:35 +0100

    fixed weird segfault in memcpy.s

--- src/memcpy.s
+++ src/memcpy.s
@@ -1,16 +1,16 @@
 #void	*ft_memcpy(void *dest, const void *src, size_t n);
-
 .global ft_memcpy
 
 ft_memcpy:
 	movq	%rdi, %rax	 # return val
+	
+	movb	%dl, %cl
 
 	testq	$0xFFFFFFFFFFFFFFF0, %rdx
-	je		if_N_less_than_16
+	je		less_than_16
 
-move_16byte_chunk:
-	mov		%rdx, %rcx
-	xorq	$0xF, %rcx //rcx for later smaller ranges
+move_16byte_chunks:
+	andb	$0xF, %cl //cl for later smaller ranges
 	shr		$4, %rdx
 
 loop16:
@@ -23,19 +23,32 @@ loop16:
 	testq	%rdx, %rdx
 	jne		loop16
 
-if_N_less_than_16:
+less_than_16:
 	testb	$248, %cl
-	je		if_N_less_than_8
+	je		less_than_8
 
 move_8byte_chunk:
-	subb	$8, %cl
-	movq	(%rsi), %r10
-	movq	%r10, (%rdi)
+	sub		$8, %cl
+	mov		(%rsi), %r10
+	mov		%r10, (%rdi)
 	add		$8, %rdi
 	add		$8, %rsi
 
-if_N_less_than_8:
-	rep		movsb		//rep: repeat movsb %rcx times
+less_than_8:
+	testb	%cl, %cl
+	je		return
+less_than_8_n0:
+	decb	%cl
+	movb	(%rsi), %r10b
+	movb	%r10b, (%rdi)
+	add		$1, %rdi
+	add		$1, %rsi
+	testb	%cl, %cl
+	jne		less_than_8_n0
+
+#this appertly segfaults sometimes?:
+//less_than_8:
+//	rep		movsb		//rep: repeat movsb %rcx times
 						//moves: moves (%rsi) to (%rdi) and increases
 						//	rsi/rdi by 1 (b(yte) size)
 						//(no other regs possible)
